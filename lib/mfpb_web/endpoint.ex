11141bcc52e6bb2dbb4b9558890736230c89cf6b
Riccardo Binetti <rbino@gmx.com>
2019-10-05 12:30:12 +0200

    endpoint: remove Plug.Parsers
    Avoid consuming the request body

--- lib/mfpb_web/endpoint.ex
+++ lib/mfpb_web/endpoint.ex
@@ -28,11 +28,6 @@ defmodule MFPBWeb.Endpoint do
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
-  plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
-    pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
-
   plug Plug.MethodOverride
   plug Plug.Head
 
