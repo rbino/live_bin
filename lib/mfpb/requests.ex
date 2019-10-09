eb07d1a9b3220b1bf70f53a0abf39076cdb1c5d9
Riccardo Binetti <rbino@gmx.com>
2019-10-09 22:39:11 +0200

    requests: add create_new_bin function

--- lib/mfpb/requests.ex
+++ lib/mfpb/requests.ex
@@ -1,6 +1,23 @@
 defmodule MFPB.Requests do
+  require Logger
+
   alias MFPB.Requests.Bin
   alias MFPB.Requests.Request
+  alias MFPB.Requests.Bin.Supervisor, as: BinSupervisor
+
+  def create_new_bin do
+    bin_id =
+      :crypto.strong_rand_bytes(10)
+      |> Base.url_encode64(padding: false)
+
+    with {:ok, _pid} <- DynamicSupervisor.start_child(BinSupervisor, {Bin, bin_id: bin_id}) do
+      {:ok, bin_id}
+    else
+      err ->
+        Logger.warn("Error while creating new bin: #{inspect(err)}")
+        :error
+    end
+  end
 
   def add_request(bin_id, %Request{} = request) when is_binary(bin_id) do
     with :ok <- Bin.append(bin_id, request),
