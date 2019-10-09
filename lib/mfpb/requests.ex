89fd8c99e63278ba7e9e8854ac5eae63587405b9
Riccardo Binetti <rbino@gmx.com>
2019-10-09 22:37:50 +0200

    requests: accept bin_id in all functions
    We now have multiple bins

--- lib/mfpb/requests.ex
+++ lib/mfpb/requests.ex
@@ -2,18 +2,18 @@ defmodule MFPB.Requests do
   alias MFPB.Requests.Bin
   alias MFPB.Requests.Request
 
-  def add_request(%Request{} = request) do
-    with :ok <- Bin.append(Bin, request),
-         :ok <- Phoenix.PubSub.broadcast(MFPB.PubSub, "requests", request) do
+  def add_request(bin_id, %Request{} = request) when is_binary(bin_id) do
+    with :ok <- Bin.append(bin_id, request),
+         :ok <- Phoenix.PubSub.broadcast(MFPB.PubSub, "requests:#{bin_id}", request) do
       :ok
     end
   end
 
-  def get_all_requests do
-    Bin.get_all(Bin)
+  def get_all_requests(bin_id) when is_binary(bin_id) do
+    Bin.get_all(bin_id)
   end
 
-  def subscribe do
-    Phoenix.PubSub.subscribe(MFPB.PubSub, "requests")
+  def subscribe(bin_id) when is_binary(bin_id) do
+    Phoenix.PubSub.subscribe(MFPB.PubSub, "requests:#{bin_id}")
   end
 end
