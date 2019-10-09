0eb3f0874d21ad4063ba3c9b6fbd7df8d40db470
Riccardo Binetti <rbino@gmx.com>
2019-10-09 22:33:31 +0200

    bin: handle multiple bins, identified with bin_id

--- lib/mfpb/requests/bin.ex
+++ lib/mfpb/requests/bin.ex
@@ -2,17 +2,24 @@ defmodule MFPB.Requests.Bin do
   use Agent
 
   alias MFPB.Requests.Request
+  alias MFPB.Requests.Bin.Registry, as: BinRegistry
 
   def start_link(args) do
-    name = Keyword.get(args, :name)
-    Agent.start_link(fn -> [] end, name: name)
+    {bin_id, _args} = Keyword.pop(args, :bin_id)
+    Agent.start_link(fn -> [] end, name: via_tuple(bin_id))
   end
 
-  def get_all(bin) do
-    Agent.get(bin, fn state -> state end)
+  def get_all(bin_id) do
+    via_tuple(bin_id)
+    |> Agent.get(fn state -> state end)
   end
 
-  def append(bin, %Request{} = request) do
-    Agent.update(bin, fn state -> [request | state] end)
+  def append(bin_id, %Request{} = request) do
+    via_tuple(bin_id)
+    |> Agent.update(fn state -> [request | state] end)
+  end
+
+  defp via_tuple(bin_id) do
+    {:via, Registry, {BinRegistry, bin_id}}
   end
 end
