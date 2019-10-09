075f8f61c32c38672641a013edd29429b3138ffb
Riccardo Binetti <rbino@gmx.com>
2019-10-09 22:46:37 +0200

    index_live: handle create event

--- lib/mfpb_web/live/index_live.ex
+++ lib/mfpb_web/live/index_live.ex
@@ -1,6 +1,7 @@
 defmodule MFPBWeb.IndexLive do
   use Phoenix.LiveView
 
+  alias MFPB.Requests
   alias MFPBWeb.IndexView
 
   def mount(_session, socket) do
@@ -9,5 +10,16 @@ defmodule MFPBWeb.IndexLive do
 
   def render(assigns) do
     IndexView.render("index.html", assigns)
+  end
+
+  def handle_event("create", _params, socket) do
+    case Requests.create_new_bin() do
+      {:ok, bin_id} ->
+        {:noreply, live_redirect(socket, to: "/b/#{bin_id}")}
+
+      :error ->
+        # TODO: handle this
+        {:noreply, socket}
+    end
   end
 end
