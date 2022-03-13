fa7ab2e51f110c8fde0a18582bc00efacea7ca2b
Riccardo Binetti <rbino@gmx.com>
2022-03-13 11:44:15 +0100

    Apply some basic css styling
    - Remove old unused classes
    - Apply some basic styling to links
    - Change the default font (TODO: load it as asset)
    - Center and pad containers by default

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,5 +1,7 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './js/**/*.js',
@@ -7,7 +9,15 @@ module.exports = {
     '../lib/*_web/**/*.*ex'
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Lato', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
